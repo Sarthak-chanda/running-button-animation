f75acb0b7d616fdf846dfd42af2a0f811ca86693
Sarthak-chanda <sarthakchanda1234@gmail.com>
2026-01-03 12:47:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -24,33 +24,43 @@ main_button.addEventListener('mouseenter', () => {
 })
 
 
-let imagearr = ['1000194714-removebg-preview.png', "IMG-20230616-WA0041.jpg", "1000194715-removebg-preview.png", 'IMG_20250211_203715_452.jpg']
-let questionarr = ['Do you know me ?', 'Do you think i am handsome ?', 'Do you love me now ?', 'Want to marry me ?']
-let background = ['https://images.unsplash.com/photo-1707924962886-12ad20367315?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D', 'https://images.unsplash.com/photo-1514519273132-6a1abd48302c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8c2t5JTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D',
-    'https://images.unsplash.com/photo-1516414559093-91c1c3d7359c?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bG92ZSUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D','https://images.unsplash.com/photo-1517867065801-e20f409696b0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGxvdmUlMjBiYWNrZ3JvdW5kfGVufDB8fDB8fHww'
-]
+let imagearr = ['IMG-20241012-WA0049.jpg','WhatsApp Image 2026-01-03 at 10.59.03 AM.jpeg',
+    'IMG-20230616-WA0041.jpg','1000059659-01.jpg'];
+let questionarr = ["Hey! I heard you're looking for a Rockstar Developer?","My code is clean, my coffee is strong. Should we talk?"
+    ,"I don't just fix bugs, I prevent them. Ready to sign the offer?","Is a 12 LPA package a fair deal for a genius?"];
 
-let step = 1;
+let background = ['https://media.istockphoto.com/id/122204403/photo/binary-code.jpg?s=2048x2048&w=is&k=20&c=VGwAYQC57ocTfmxsiebDjlEZymC_rM7HKmvdonG8qmg=',
+    'https://images.unsplash.com/photo-1629233321694-62ee7a57a775?q=80&w=1173&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://media.istockphoto.com/id/1267593738/photo/successful-partnership.jpg?s=2048x2048&w=is&k=20&c=83F0AREuJp6uCoRrQOvpb2vMea7xosbkLsljtryG8_4=',
+    'https://images.unsplash.com/photo-1724754202864-164ab53de85e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D']
+
+let step = 0;
+pic.src = imagearr[step];
+question.textContent = questionarr[step];
+maincontainer.style.backgroundImage = `url('${background[step]}')`;
+
+
 sub_button.addEventListener('click', () => {
+    ++step;
     question.classList.add('hidden-bottom');
     pic.classList.add('hidden-left');
    setTimeout(()=>{ 
     if (step < imagearr.length) {
+    
         pic.src = imagearr[step];
         question.textContent = questionarr[step];
         maincontainer.style.backgroundImage = `url('${background[step]}')`;
-        step++;
+        
     }
     else {
-        pic.src = 'IMG-20230616-WA0034.jpg';
-        pic.style.scale = '2';
-        question.textContent = 'Sry I have a GF !!!';
+        pic.src = 'WhatsApp Image 2026-01-03 at 10.12.16 AM.jpeg';
+        pic.style.scale = '1.5';
+        question.textContent = "Great! See you at stand-up tomorrow morning! 🚀";
         question.style.color = '#ff1515';
         question.style.textShadow = '1px -1px 1px #000';
         question.style.scale = '2';
-        question_box.style.scale = '2';
         answer.style.display = 'none';
-        maincontainer.style.backgroundImage = `url('https://plus.unsplash.com/premium_photo-1730063032585-216d03972ae1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjV8fGZpcmUlMjBiYWNrZ3JvdW5kfGVufDB8fDB8fHww')`;
+        maincontainer.style.backgroundImage = `url('https://images.unsplash.com/photo-1712188767260-f4ffcfa7f308?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`;
     }
 
         question.classList.remove('hidden-bottom');
